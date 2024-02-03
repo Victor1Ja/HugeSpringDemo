c15ea1f46c8e268aafe7c2c603ac44123bec7514
Victor Lopez <victor.98.javier@gmail.com>
2024-02-03 13:43:05 +0100

    Initial Commit

---
+++ src/main/java/me/vitocode/hs/javademo/demo/DemoApplication.java
@@ -0,0 +1,31 @@
+package me.vitocode.hs.javademo.demo;
+
+import java.text.Annotation;
+import java.util.jar.Attributes.Name;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@SpringBootApplication
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+
+	}
+
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(@SuppressWarnings("null") CorsRegistry registry) {
+				registry.addMapping("/*").allowedOrigins("*");
+			}
+		};
+	}
+
+}
---
+++ src/test/java/me/vitocode/hs/javademo/demo/DemoApplicationTests.java
@@ -0,0 +1,15 @@
+package me.vitocode.hs.javademo.demo;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
